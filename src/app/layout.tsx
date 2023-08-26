9b0ad9342c1c36876e87bf9cd7fe43a0a6d0d3e0
alexxtomas <atomasllimos@gmail.com>
2023-08-26 20:26:36 +0200

    style: Add styles to body layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/utils/functions'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -12,7 +13,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='es'>
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          'mx-auto h-screen w-screen bg-zinc-900 max-w-5xl flex flex-col items-center  text-white gap-10 relative py-28 md:py-20',
+          inter.className
+        )}
+      >
+        {children}
+      </body>
     </html>
   )
 }
