eb0b3294a56c525a5c9b5c5289238467e68a24a8
alexxtomas <atomasllimos@gmail.com>
2023-08-27 09:43:19 +0200

    feat: Start createing ConfigPage

---
+++ src/app/config/page.tsx
@@ -0,0 +1,11 @@
+import FormFieldDropdownVariant from '@/components/form-field/form-field-dropdown-variant'
+
+export default function ConfigPage() {
+  return (
+    <>
+      <form className='flex flex-col items-center justify-center w-full h-full'>
+        <FormFieldDropdownVariant />
+      </form>
+    </>
+  )
+}
