63822c0a016edb44fa00b6274759c242b9214812
alexxtomas <atomasllimos@gmail.com>
2023-08-27 09:41:15 +0200

    chore: Create file to save fonts and constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,44 @@
+import { type Props as CardProps } from '@/components/card'
+
+export const PRE_ESTABLISHED_CONFIGURATIONS: CardProps[] = [
+  {
+    img: {
+      src: '/images/landing.webp',
+      alt: 'Landing page image'
+    },
+    description:
+      'Configuración la cual utiliza las tecnologías más punteras para crear tu Landing page de manera rapida y eficiente',
+    href: '/landing-page',
+    title: 'Landing Page'
+  },
+  {
+    img: {
+      src: '/images/landing.webp',
+      alt: 'Landing page image'
+    },
+    description:
+      'Configuración la cual utiliza las tecnologías más punteras para crear tu Landing page de manera rapida y eficiente',
+    href: '/landing-page',
+    title: 'Landing Page'
+  },
+  {
+    img: {
+      src: '/images/landing.webp',
+      alt: 'Landing page image'
+    },
+    description:
+      'Configuración la cual utiliza las tecnologías más punteras para crear tu Landing page de manera rapida y eficiente',
+    href: '/landing-page',
+    title: 'Landing Page'
+  },
+  {
+    img: {
+      src: '/images/landing.webp',
+      alt: 'Landing page image'
+    },
+    description:
+      'Configuración la cual utiliza las tecnologías más punteras para crear tu Landing page de manera rapida y eficiente',
+    href: '/landing-page',
+    title: 'Landing Page'
+  }
+]
---
+++ src/utils/fonts.ts
@@ -0,0 +1,5 @@
+import { Montserrat } from 'next/font/google'
+import { Inter } from 'next/font/google'
+
+export const MONTSERRAT = Montserrat({ subsets: ['latin'] })
+export const INTER = Inter({ subsets: ['latin'] })
