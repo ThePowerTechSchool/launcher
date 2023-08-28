2d70ed3685f266ffa8ac24c8457d5af1ae92dd23
alexxtomas <atomasllimos@gmail.com>
2023-08-28 20:10:07 +0200

    style: Change form field components styles

--- src/components/form-field/form-field-label.tsx
+++ src/components/form-field/form-field-label.tsx
@@ -6,7 +6,7 @@ export default function FormFieldLabel({
   children: React.ReactNode
 }) {
   return (
-    <label className='block text-[20px] max-w line-clamp-[28px] font-sans' htmlFor={htmlFor}>
+    <label className='block text-[20px] line-clamp-[28px font-sans' htmlFor={htmlFor}>
       {children}
     </label>
   )
--- src/components/form-field/form-field-wraper.tsx
+++ src/components/form-field/form-field-wraper.tsx
@@ -1,3 +1,3 @@
 export default function FormFieldWraper({ children }: { children: React.ReactNode }) {
-  return <section className='w-[310px] flex flex-col gap-5'>{children}</section>
+  return <section className='p-10 w-full flex flex-col gap-5'>{children}</section>
 }
