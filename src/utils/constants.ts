5aa0a1001273d7ea420bb93dd156e0e188118779
alexxtomas <atomasllimos@gmail.com>
2023-08-28 20:09:20 +0200

    feat: Create constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 import { type Props as CardProps } from '@/components/card'
+import type { DropdownElement } from '@/components/form-field/form-field-dropdown'
 
 export const PRE_ESTABLISHED_CONFIGURATIONS: CardProps[] = [
   {
@@ -40,5 +41,26 @@ export const PRE_ESTABLISHED_CONFIGURATIONS: CardProps[] = [
       'Configuración la cual utiliza las tecnologías más punteras para crear tu Landing page de manera rapida y eficiente',
     href: '/landing-page',
     title: 'Landing Page'
+  }
+]
+
+export const TECHNOLOGIES: DropdownElement[] = [
+  {
+    name: 'React',
+    className: 'text-blue-300 border-blue-300 z-20',
+    higilightedColor: 'blue',
+    iconColor: 'fill-blue-500'
+  },
+  {
+    name: 'JavaScript',
+    className: 'text-yellow-300 border-yellow-300 z-20',
+    higilightedColor: 'yellow',
+    iconColor: 'fill-yellow-500'
+  },
+  {
+    name: 'Node.js',
+    className: 'text-green-300 border-green-300 z-20',
+    higilightedColor: 'green',
+    iconColor: 'fill-green-500'
   }
 ]
