5e22f854ca89adef287839104ab9e4fd6b510a10
alexxtomas <atomasllimos@gmail.com>
2023-08-28 20:10:52 +0200

    chore: Add new props

--- src/components/form-field/form-field-dropdown-element.tsx
+++ src/components/form-field/form-field-dropdown-element.tsx
@@ -1,19 +1,25 @@
 import { cn } from '@/utils/functions'
 
 interface Props {
-  color: string
+  className?: string
   children: React.ReactNode
+  handleClick?: () => void
+  // color: keyof typeof COLORS
 }
 
-export default function FormFieldDropdownElement({ color, children }: Props) {
-  const borderColor = `border-${color}`
-  const textColor = `text-${color}`
+export const COLORS = {
+  yellow: 'text-yellow-300 border-yellow-300 fill-yellow-500',
+  blue: 'text-blue-300 border-blue-300 fill-blue-500',
+  green: 'text-green-300 border-green-300 fill-green-500'
+}
+
+export default function FormFieldDropdownElement({ className, children, handleClick }: Props) {
   return (
     <li
+      onClick={handleClick}
       className={cn(
-        'py-2 px-2  border rounded-lg hover:scale-95 transition-transform duration-300 ease-in-out cursor-pointer',
-        borderColor,
-        textColor
+        'py-2 pl-2 pr-4  border rounded-lg hover:scale-95 transition-transform duration-300 ease-in-out cursor-pointer flex items-center justify-between',
+        className
       )}
     >
       {children}
