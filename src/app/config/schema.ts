4a5807cbffe11f1aa757defcceacbd4f052104b9
alexxtomas <atomasllimos@gmail.com>
2023-08-30 12:42:45 +0200

    feat: Create new schemas

--- src/app/config/schema.ts
+++ src/app/config/schema.ts
@@ -1,5 +1,11 @@
-import { maxLength, minLength, string } from 'valibot'
-import { AVAILABLE_TECHNOLOGIES, PROJECT_NAME_MAX_LENGTH } from './constants'
+import { custom, maxLength, minLength, string } from 'valibot'
+import {
+  AVAILABLE_FRONT_VARIANTS,
+  AVAILABLE_NODEJS_VARIANTS,
+  AVAILABLE_STYLING_VARIANTS,
+  AVAILABLE_TECHNOLOGIES,
+  PROJECT_NAME_MAX_LENGTH
+} from './constants'
 
 export const projectNameSchema = string([
   minLength(1, 'El nombre del proyecto no puede estar vacío'),
@@ -8,16 +14,28 @@ export const projectNameSchema = string([
 
 export const technologySchema = string([
   minLength(1, 'La tecnología no puede estar vacía'),
-  (input) => {
-    if (!AVAILABLE_TECHNOLOGIES.includes(input)) {
-      return {
-        issue: {
-          validation: 'custom',
-          message: 'La tecnología que has elegido no está disponible',
-          input
-        }
-      }
-    }
-    return { output: input }
-  }
+  custom((input) => {
+    return AVAILABLE_TECHNOLOGIES.includes(input)
+  }, 'La tecnología que has elegido no está disponible')
+])
+
+export const frontVariantSchema = string([
+  minLength(1, 'La variante no puede estar vacía'),
+  custom((input) => {
+    return AVAILABLE_FRONT_VARIANTS.includes(input)
+  }, 'La variante que has elegido no está disponible')
+])
+
+export const nodejsVariantSchema = string([
+  minLength(1, 'La variante no puede estar vacía'),
+  custom((input) => {
+    return AVAILABLE_NODEJS_VARIANTS.includes(input)
+  }, 'La variante que has elegido no está disponible')
+])
+
+export const stylingVariantSchema = string([
+  minLength(1, 'La variante no puede estar vacía'),
+  custom((input) => {
+    return AVAILABLE_STYLING_VARIANTS.includes(input)
+  }, 'La variante que has elegido no está disponible')
 ])
