aeb39a11149b07641d13c7d4dba99f23357969a7
alexxtomas <atomasllimos@gmail.com>
2023-09-04 11:33:36 +0200

    feat: Change config form hook logic

--- src/components/config-form/index.tsx
+++ src/components/config-form/index.tsx
@@ -1,149 +1,44 @@
 'use client'
 import type { FormFieldDropdownPropsExtended, FormFieldTextPropsExtended } from './types'
-import { useEffect, useState } from 'react'
-import { CONFIG_FORM_FIELD_VARIANTS } from './constants'
-import type FormFieldText from '@/components/form-field/form-field-text'
-import { type Props as FormFieldTextProps } from '@/components/form-field/form-field-text'
-import type FormFieldDropdown from '../form-field/form-field-dropdown'
-import { type Props as FormFieldDropdownProps } from '../form-field/form-field-dropdown'
 import ConfigFormNav from '../config-form-nav'
+import { useConfigForm } from '@/hooks/use-config-form'
 
 export interface Props {
   formFields: Array<FormFieldDropdownPropsExtended | FormFieldTextPropsExtended>
   onSubmit: (values: { [key: string]: string }) => void
 }
 
-const INITIAL_STEP = 0
-
 export default function ConfigForm({ formFields, onSubmit }: Props) {
-  const [steps, setSteps] = useState<number[]>([INITIAL_STEP])
-  const [values, setValues] = useState<{ [key: string]: string }>(() => {
-    const keys = formFields.map(({ id }) => id)
-
-    return keys.reduce((acc, key) => {
-      return Object.assign(acc, { [key]: '' })
-    }, {})
-  })
-  const [sendForm, setSendForm] = useState(false)
-
-  const [formField, setFormField] = useState<
-    FormFieldDropdownPropsExtended | FormFieldTextPropsExtended
-  >(formFields[INITIAL_STEP])
-
-  const { variant, formFields: nestedFormFields, ...restOfFormFieldProps } = formField
-
-  let FormField
-  let _restOfFormFieldProps
-
-  switch (variant) {
-    case 'dropdown': {
-      FormField = CONFIG_FORM_FIELD_VARIANTS[variant] as typeof FormFieldDropdown
-      _restOfFormFieldProps = restOfFormFieldProps as FormFieldDropdownProps
-    }
-    case 'text': {
-      FormField = CONFIG_FORM_FIELD_VARIANTS[variant] as typeof FormFieldText
-      _restOfFormFieldProps = restOfFormFieldProps as FormFieldTextProps
-    }
-  }
-
-  const onSuccess = (value: string, id: string) => {
-    const newValues = { ...values, [id]: value }
-    setValues(newValues)
-
-    if (Array.isArray(nestedFormFields)) {
-      const { length } = nestedFormFields
-      if (length === 1) {
-        const newSteps = [...steps, 0]
-        const newFormField = nestedFormFields[0]
-        setSteps(newSteps)
-        setFormField(newFormField)
-
-        return
-      }
-
-      const nextFormFieldIdx = nestedFormFields.findIndex((formField) => {
-        return (
-          formField.if &&
-          Object.entries(formField.if).every(([key, value]) => {
-            return value.includes(newValues[key])
-          })
-        )
-      })
-
-      const newFormField = nestedFormFields[nextFormFieldIdx]
-
-      setSteps([...steps, nextFormFieldIdx])
-      setFormField(newFormField)
-
-      return
-    }
-    // !! test if steps.length is bigger than 1
-    const newSteps = [...steps]
-    newSteps[newSteps.length - 1] = newSteps[newSteps.length - 1] + 1
-    setSteps(newSteps)
-
-    if (newSteps.length === 1) {
-      setFormField(formFields[newSteps[0]])
-      return
-    }
-
-    let buffer = formFields[steps[0]]
-
-    for (let i = 1; i < newSteps.length; i++) {
-      buffer = buffer?.formFields?.[steps[i]] as
-        | FormFieldDropdownPropsExtended
-        | FormFieldTextPropsExtended
-    }
-
-    setFormField(buffer)
-  }
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    onSubmit(values)
-  }
-
-  useEffect(() => {
-    let buffer = formFields[steps[0]]
-    const newSteps = [...steps]
-    newSteps[newSteps.length - 1] = newSteps[newSteps.length - 1] + 1
-
-    for (let i = 1; i < newSteps.length; i++) {
-      buffer = buffer?.formFields?.[newSteps[i]] as
-        | FormFieldDropdownPropsExtended
-        | FormFieldTextPropsExtended
-    }
-    if (!nestedFormFields && !buffer) {
-      setSendForm(true)
-    }
-  }, [formField, steps])
-
+  const { FormField, formFieldProps, handleKeyDown, handleSubmit, onSuccess, sendForm } =
+    useConfigForm({ formFields, onSubmit })
   return (
     <form
+      onKeyDown={handleKeyDown}
       onSubmit={handleSubmit}
       className='flex flex-col items-center justify-center md:justify-center w-full h-full'
     >
       <FormField
         buttonType={sendForm ? 'submit' : 'button'}
-        {..._restOfFormFieldProps}
+        {...formFieldProps}
         onSuccess={onSuccess}
       />
+      {/* TODO: Arrows logic  */}
       <ConfigFormNav
-        arrowDownButton={{
-          disabled: true,
-          handleClick: () => {}
-        }}
-        arrowUpButton={{
-          disabled: true,
-          handleClick: () => {}
-        }}
+        // arrowDownButton={{
+        //   disabled: true,
+        //   handleClick: () => {}
+        // }}
+        // arrowUpButton={{
+        //   disabled: true,
+        //   handleClick: () => {}
+        // }}
         link={{
           href: 'https://thepowermba.com/',
           target: '_blank',
           rel: 'noopener noreferrer'
         }}
       >
-        Powered by <span className='text-black font-bold'>The Power MBA</span>
+        Powered by <span className='font-bold '>The Power MBA</span>
       </ConfigFormNav>
     </form>
   )
--- src/hooks/use-config-form.tsx
+++ src/hooks/use-config-form.tsx
@@ -1,12 +1,17 @@
-import type { FormEvent } from 'react'
-import { useState } from 'react'
-import type { Props as ConfigFormProps } from '@/components/config-form'
 import { CONFIG_FORM_FIELD_VARIANTS } from '@/components/config-form/constants'
-import { parse } from 'valibot'
+import type {
+  FormFieldDropdownPropsExtended,
+  FormFieldTextPropsExtended
+} from '@/components/config-form/types'
+import type FormFieldDropdown from '@/components/form-field/form-field-dropdown'
+import type FormFieldText from '@/components/form-field/form-field-text'
+import { type Props as FormFieldDropdownProps } from '@/components/form-field/form-field-dropdown'
+import { type Props as FormFieldTextProps } from '@/components/form-field/form-field-text'
+import type { Props as ConfigFormProps } from '@/components/config-form'
+import { useState, useEffect } from 'react'
 
-export function useConfigForm({ formFields }: ConfigFormProps) {
-  const [step, setStep] = useState(0)
-  const [errorMessage, setErrorMessage] = useState('')
+export function useConfigForm({ formFields, onSubmit }: ConfigFormProps) {
+  const [steps, setSteps] = useState<number[]>([0])
   const [values, setValues] = useState<{ [key: string]: string }>(() => {
     const keys = formFields.map(({ id }) => id)
 
@@ -14,75 +19,112 @@ export function useConfigForm({ formFields }: ConfigFormProps) {
       return Object.assign(acc, { [key]: '' })
     }, {})
   })
+  const [sendForm, setSendForm] = useState(false)
 
-  const formField = formFields[step]
+  const [formField, setFormField] = useState<
+    FormFieldDropdownPropsExtended | FormFieldTextPropsExtended
+  >(formFields[0])
 
-  const FormField = CONFIG_FORM_FIELD_VARIANTS[formField.variant]
+  const { variant, formFields: nestedFormFields, ...restOfFormFieldProps } = formField
 
-  const handleArrowUpClick = () => {
-    setStep(step + 1)
-  }
+  let FormField
+  let _restOfFormFieldProps
 
-  const handleArrowDownClick = () => {
-    setStep(step - 1)
-  }
-
-  const clear = () => {
-    setErrorMessage('')
-  }
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const formData = new FormData(e.currentTarget)
-    const formField = formFields[step]
-    const data = Object.fromEntries(formData)
-
-    const formFieldData = data[formField.id]
-
-    try {
-      parse(formField.schema, formFieldData)
-      setErrorMessage('')
-      const newValues = { ...values, [formField.id]: formFieldData as string }
-      setValues(newValues)
-      if (step !== formFields.length - 1) {
-        let nextStep = step + 1
-        const nextFormField = formFields[nextStep]
-
-        if (nextFormField.if) {
-          const ifEntries = Object.entries(nextFormField.if)
-          let isIfValid = false
-
-          do {
-            const [ifKey, ifValue] = ifEntries[0]
-
-            if (ifValue.includes(newValues[ifKey])) {
-              isIfValid = true
-            } else {
-              nextStep++
-            }
-            if (nextStep === formFields.length - 1) {
-              break
-            }
-          } while (!isIfValid)
-        }
-        setStep(nextStep)
-        setErrorMessage('')
-      }
-    } catch (err) {
-      const _err = err as Error
-      setErrorMessage(_err.message)
-      return
+  switch (variant) {
+    case 'dropdown': {
+      FormField = CONFIG_FORM_FIELD_VARIANTS[variant] as typeof FormFieldDropdown
+      _restOfFormFieldProps = restOfFormFieldProps as FormFieldDropdownProps
+    }
+    case 'text': {
+      FormField = CONFIG_FORM_FIELD_VARIANTS[variant] as typeof FormFieldText
+      _restOfFormFieldProps = restOfFormFieldProps as FormFieldTextProps
     }
   }
+
+  const onSuccess = (value: string, id: string) => {
+    const newValues = { ...values, [id]: value }
+    setValues(newValues)
+
+    if (Array.isArray(nestedFormFields)) {
+      const { length } = nestedFormFields
+      if (length === 1) {
+        const newSteps = [...steps, 0]
+        const newFormField = nestedFormFields[0]
+        setSteps(newSteps)
+        setFormField(newFormField)
+
+        return
+      }
+
+      const nextFormFieldIdx = nestedFormFields.findIndex((formField) => {
+        return (
+          formField.if &&
+          Object.entries(formField.if).every(([key, value]) => {
+            return value.includes(newValues[key])
+          })
+        )
+      })
+
+      const newFormField = nestedFormFields[nextFormFieldIdx]
+
+      setSteps([...steps, nextFormFieldIdx])
+      setFormField(newFormField)
+
+      return
+    }
+    // !! test if steps.length is bigger than 1
+    const newSteps = [...steps]
+    newSteps[newSteps.length - 1] = newSteps[newSteps.length - 1] + 1
+    setSteps(newSteps)
+
+    if (newSteps.length === 1) {
+      setFormField(formFields[newSteps[0]])
+      return
+    }
+
+    let buffer = formFields[steps[0]]
+
+    for (let i = 1; i < newSteps.length; i++) {
+      buffer = buffer?.formFields?.[steps[i]] as
+        | FormFieldDropdownPropsExtended
+        | FormFieldTextPropsExtended
+    }
+
+    setFormField(buffer)
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    onSubmit(values)
+  }
+
+  useEffect(() => {
+    let buffer = formFields[steps[0]]
+    const newSteps = [...steps]
+    newSteps[newSteps.length - 1] = newSteps[newSteps.length - 1] + 1
+
+    for (let i = 1; i < newSteps.length; i++) {
+      buffer = buffer?.formFields?.[newSteps[i]] as
+        | FormFieldDropdownPropsExtended
+        | FormFieldTextPropsExtended
+    }
+    if (!nestedFormFields && !buffer) {
+      setSendForm(true)
+    }
+  }, [formField, steps])
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+    }
+  }
+
   return {
-    step,
-    errorMessage,
-    values,
-    formField,
     FormField,
-    handleArrowUpClick,
-    handleArrowDownClick,
+    formFieldProps: _restOfFormFieldProps,
+    handleKeyDown,
     handleSubmit,
-    clear
+    onSuccess,
+    sendForm
   }
 }
