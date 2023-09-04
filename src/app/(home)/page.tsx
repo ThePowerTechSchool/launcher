4b37188701af6e67e029f9500e5ad203a0b00d8e
alexxtomas <atomasllimos@gmail.com>
2023-09-04 13:28:21 +0200

    feat: Use constants

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,10 @@
-import { HIGILIGHTED_FEATURES, PROJECT_NAME } from '@/utils/constants'
+import { PROJECT_NAME } from '@/utils/constants'
 import HomeHeader from '@/components/home-header'
 import { HOME_HEADER_PROPS } from './props'
 import LandingParragraph from '@/components/lading-parragraph'
 import LandingTitle from '@/components/landing-title'
 import Link from 'next/link'
+import { CALL_TO_ACTION, HIGHLIGHTED_FEATURES, INTRODUCTION_TEXT, WHAT_IS } from './constants'
 
 export default function Home() {
   return (
@@ -11,24 +12,12 @@ export default function Home() {
       <HomeHeader {...HOME_HEADER_PROPS} />
       <main>
         <section className='px-4'>
-          <LandingParragraph>
-            ¡Bienvenido a la revolución en el desarrollo web! {PROJECT_NAME} es tu socio perfecto
-            para acelerar tus proyectos en línea. ¿Estás cansado de perder tiempo configurando tu
-            entorno de desarrollo, instalando dependencias y creando estructuras de proyectos desde
-            cero? ¡No busques más! {PROJECT_NAME} ha llegado para simplificar tu vida y hacerte más
-            productivo que nunca.
-          </LandingParragraph>
-          <LandingTitle>¿Qué es {PROJECT_NAME}?</LandingTitle>
-          <LandingParragraph>
-            {PROJECT_NAME} es un innovador acelerador de proyectos web que te permite iniciar tus
-            desarrollos con un solo clic. Ya no tienes que preocuparte por las tareas tediosas de
-            configuración y organización. Nuestra aplicación inteligente hace el trabajo por ti,
-            permitiéndote enfocarte en lo que realmente importa: escribir código y hacer que tu
-            proyecto cobre vida.
-          </LandingParragraph>
-          <LandingTitle>Características Destacadas:</LandingTitle>
-          <ol className='list-decimal px-4'>
-            {HIGILIGHTED_FEATURES.map((feature) => {
+          <LandingParragraph>{INTRODUCTION_TEXT}</LandingParragraph>
+          <LandingTitle>{WHAT_IS.title}</LandingTitle>
+          <LandingParragraph>{WHAT_IS.parragraph}</LandingParragraph>
+          <LandingTitle>{HIGHLIGHTED_FEATURES.title}</LandingTitle>
+          <ol data-testid='higilighted-features-ordered-list' className='list-decimal px-4'>
+            {HIGHLIGHTED_FEATURES.features.map((feature) => {
               return (
                 <li key={feature.title} className='first-of-type:mt-0 mt-6'>
                   <LandingParragraph>
@@ -46,20 +35,22 @@ export default function Home() {
             </span>
             Ahora!
           </LandingTitle>
-          <div className='flex flex-wrap mb-10 items-center justify-center w-full mt-10 gap-10'>
-            <Link
-              href={'/config'}
-              className='text-black bg-gradient-to-r from-zinc-100 via-zinc-200 to-zinc-300 hover:bg-gradient-to-br hover:from-zinc-300 hover:via-zinc-200 hover:to-zinc-100   shadow-lg shadow-zinc-500/50  rounded-lg text-md px-5 py-2.5 text-center mr-2 mb-2 ring-1 animate-bounce  font-medium text-lg md:text-xl '
-            >
-              Crear Configuración
-            </Link>
-            <Link
-              href={'/pre-established-config'}
-              className='text-black bg-gradient-to-r from-zinc-100 via-zinc-200 to-zinc-300 hover:bg-gradient-to-br hover:from-zinc-300 hover:via-zinc-200 hover:to-zinc-100   shadow-lg shadow-zinc-500/50  rounded-lg text-md px-5 py-2.5 text-center mr-2 mb-2 ring-1 animate-bounce  font-medium text-lg md:text-xl '
-            >
-              Usar Configuración
-            </Link>
-          </div>
+          <ul
+            data-testid='call-to-action-unordered-list'
+            className='flex flex-wrap mb-10 items-center justify-center w-full mt-10 gap-10'
+          >
+            {CALL_TO_ACTION.links.map(({ href, text }) => {
+              return (
+                <Link
+                  key={href}
+                  href={href}
+                  className='text-black bg-gradient-to-r from-zinc-100 via-zinc-200 to-zinc-300 hover:bg-gradient-to-br hover:from-zinc-300 hover:via-zinc-200 hover:to-zinc-100   shadow-lg shadow-zinc-500/50  rounded-lg text-md px-5 py-2.5 text-center mr-2 mb-2 ring-1 animate-bounce  font-medium text-lg md:text-xl '
+                >
+                  {text}
+                </Link>
+              )
+            })}
+          </ul>
         </section>
       </main>
     </>
